3b72c4742c56b37c1b3c77afcea18ca2cb701f74
mattes432 <mattes432@806f597e-33e4-45ba-ad29-9b22a0300992>
2013-01-24 23:34:53 +0000

    initial version
    git-svn-id: svn://svn.code.sf.net/p/npplauncher/code/trunk@2 806f597e-33e4-45ba-ad29-9b22a0300992

---
+++ NppLauncher.cpp
@@ -0,0 +1,242 @@
+//----------------------------------------------------------------------------
+// Description : lanceur pour Notepad++, pour remplacer le notepad.exe de Windows
+// First Auteur   : Stepho (http://superstepho.free.fr/)
+// Création : 2005-08-22
+// Second Author  : Mattes H. mattesh(at)gmx.net
+// Creation : 2013-01-13
+// Additions done to suspend the task as long as the notepad++ is open or file editing is 
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//----------------------------------------------------------------------------
+
+#include <windows.h>
+#include <winreg.h>
+#include <stdio.h>
+#include <string.h>
+#include <shellapi.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ERRNO.H>
+
+#define CAPTION "NppLauncher 0.9.3 - "
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nFunsterStil)
+{     
+   PROCESS_INFORMATION oProcessInfo;
+   STARTUPINFOA si;
+   const int iCmdLen = 1024;
+   CHAR cmd[iCmdLen] = "\"C:\\Program Files\\Notepad++";
+   DWORD bWaitForNotepadClose=1;
+   DWORD uWaitTime=0;
+   DWORD bDebug=0;
+   // récupération du chemin d'accès de Notepad++ dans le base de registre
+   HKEY hKey;
+   DWORD i;  
+   if( (i = RegOpenKeyExA( HKEY_LOCAL_MACHINE, "SOFTWARE\\NppLauncher", 0, KEY_READ | KEY_WOW64_64KEY, &hKey )) == ERROR_SUCCESS )
+   {
+      DWORD   iType;
+      unsigned char sData[iCmdLen];
+      DWORD   iDataSize = iCmdLen;
+
+      if( RegQueryValueExA( hKey, "cmd", NULL, &iType, sData, &iDataSize )
+         == ERROR_SUCCESS && iType == REG_SZ)
+      {
+         // we let also the name of the program so that user can have different versions
+         // of the notepad
+         strcpy_s(cmd, iCmdLen, (char*)sData);
+      } 
+      else if( RegQueryValueExA( hKey, "", NULL, &iType, sData, &iDataSize )
+         == ERROR_SUCCESS && iType == REG_SZ)
+      {
+         // thet old writing becomes converted if found and then we remove it
+         if(strlen((char*)sData)>0) 
+         {
+            strcpy(cmd, "\"");
+            strcat(cmd, (char*)sData);
+         }
+         strcat(cmd, "\\notepad++.exe\"");
+      }
+
+      // Flag for waiting or not...
+      if( RegQueryValueExA( hKey, "WaitForClose", NULL, &iType, sData, &iDataSize )
+         == ERROR_SUCCESS && iType == REG_SZ)
+      {
+         bWaitForNotepadClose = (0==strcmp((char*)sData, "yes"));
+      }
+
+      // timeout for waiting ...
+      iDataSize = sizeof(uWaitTime);
+      if( RegQueryValueExA( hKey, "WaitTimeMsec", NULL, &iType, (LPBYTE)&uWaitTime, &iDataSize )
+         == ERROR_SUCCESS && iType == REG_DWORD)
+      {
+        // all fine
+      } else {
+         uWaitTime = 100;
+      }
+      // Debugging or not...
+      if( RegQueryValueExA( hKey, "Debug", NULL, &iType, sData, &iDataSize )
+         == ERROR_SUCCESS && iType == REG_SZ)
+      {
+         bDebug = (0==strcmp((char*)sData, "yes"));
+      }
+
+      RegCloseKey(hKey);
+   } else { 
+      sprintf(cmd, "open registry caused error %d", i);
+      MessageBoxA(NULL, cmd, "Error opening registry", MB_OK);
+      return 0;
+   }
+   // different calling conventions
+   LPSTR notepad1 = "NOTEPAD.EXE "; 
+   LPSTR notepad2 = "NOTEPAD.EXE\" "; 
+   LPSTR notepad3 = "NOTEPAD.EXE\""; 
+   LPSTR notepad4 = "NOTEPAD.EXE"; 
+   LPSTR notepad5 = "NOTEPAD "; 
+   LPSTR notepad6 = "NOTEPAD\" "; 
+   LPSTR notepad7 = "NOTEPAD\""; 
+   LPSTR notepad8 = "NOTEPAD"; 
+   LPSTR notepad;
+
+   LPSTR lpszMyArgument;
+   CHAR upperApp[iCmdLen];
+   if(bDebug) { 
+	  LPSTR cp = (*lpszArgument)?lpszArgument:"\"\"";
+      if(IDCANCEL == MessageBoxA(NULL, cp, "NppLauncher initial command line", MB_OKCANCEL))
+         return 0; 
+   }
+   
+   strncpy(upperApp, lpszArgument, iCmdLen);
+   upperApp[iCmdLen-1]=0;
+
+   _strupr(upperApp); // win is agnostic to the case
+   notepad = notepad1;
+   LPSTR ch = strstr(upperApp, notepad);
+   if(ch == NULL) { notepad = notepad2; ch = strstr(upperApp, notepad); }
+   if(ch == NULL) { notepad = notepad3; ch = strstr(upperApp, notepad); }
+   if(ch == NULL) { notepad = notepad4; ch = strstr(upperApp, notepad); }
+   if(ch == NULL) { notepad = notepad5; ch = strstr(upperApp, notepad); }
+   if(ch == NULL) { notepad = notepad6; ch = strstr(upperApp, notepad); }
+   if(ch == NULL) { notepad = notepad7; ch = strstr(upperApp, notepad); }
+   if(ch == NULL) { notepad = notepad8; ch = strstr(upperApp, notepad); }
+
+   size_t arglen = strlen(lpszArgument);
+   if(ch != NULL) {
+      // the first parameter is the notepad.exe we will remove this if some remainder is behind
+      ch += strlen(notepad);
+      if (arglen >= (size_t)(ch - upperApp)) {
+         lpszMyArgument = lpszArgument + (ch - upperApp);
+      } else {
+	     lpszMyArgument = lpszArgument;
+      }
+   } else {
+	  lpszMyArgument = lpszArgument;
+   }
+   // remove preceding spaces
+   while((*lpszMyArgument) &&  // not EOS
+         (lpszMyArgument < (lpszMyArgument + arglen)) && // not past max len
+         (*lpszMyArgument==' ')) { // still space
+      lpszMyArgument++;
+   }
+   // construction de la ligne de commande
+   if( strlen(lpszMyArgument) )
+   {
+      CHAR arg[iCmdLen];
+      arg[0] =0;
+      // remove quotes to call _fullpath()
+      if( lpszMyArgument[0] != '\"' )
+      {
+         strncat(arg, lpszMyArgument, iCmdLen-2);
+         arg[iCmdLen-2] = 0;
+      }
+      else
+      {
+         strncat(arg, lpszMyArgument+1, iCmdLen-3);
+         arg[iCmdLen-3] = 0;
+      }
+      _fullpath( arg, arg, iCmdLen);
+      struct _stat st;
+      memset(&st, 0, sizeof(st));
+      if(arg[strlen(arg)-1] == '\\') {
+         arg[strlen(arg)-1] = 0;
+      }
+      int ret = _stat(arg,&st);
+      const char* szerr = 0;
+
+      if(!ret && (st.st_mode &_S_IFDIR )!=0){
+         size_t len = strlen(arg)+1;
+         strncat(arg, "\\.txt", iCmdLen-len);
+         arg[iCmdLen-1] = 0;
+      } else {
+         switch (errno) {
+            case ENOENT: szerr = "ENOENT"; break;
+            case EINVAL: szerr = "EINVAL"; break;
+            default: break;
+         }; // switch
+      }
+      if(bDebug) { 
+         if(IDCANCEL == MessageBoxA(NULL, arg, "NppLauncher expanded parameters", MB_OKCANCEL)) 
+            return 0; 
+      }
+      if(strlen(arg)){
+         strcat(cmd, " \""); // separate from call and add quotes 
+         size_t len = iCmdLen-strlen(cmd);
+         strncat(cmd, arg, len);
+         cmd[iCmdLen-1] = 0;
+         strcat(cmd, "\""); // closing quotes
+      }
+   }
+
+   // préparation des informations de lancement
+   memset( &si, 0, sizeof(si) );
+   si.cb = sizeof(si);
+   memset( &oProcessInfo, 0, sizeof(oProcessInfo) );
+
+   if(bDebug) { 
+	   if(IDCANCEL == MessageBoxA(NULL, cmd, "Command to be called", MB_OKCANCEL)) 
+		   return 0; 
+   }
+
+   // lancement de Notepad++
+   if(0==CreateProcessA( NULL, cmd, NULL, NULL, false, 0, NULL, NULL, &si, &oProcessInfo))
+   {
+      DWORD d = GetLastError();
+      switch(d) {
+         case 0:
+          default: 
+             MessageBoxA( NULL, "Error in creating process. Please check registry HKLM/Software/NppLauncher/", cmd, 0);
+      }
+   } else {
+      if(bWaitForNotepadClose){
+         DWORD t = GetTickCount();
+         // Wait until child process exits.
+         WaitForSingleObject( oProcessInfo.hProcess, INFINITE );
+         if ((GetTickCount() - t ) < uWaitTime) {
+            // we have been falling through because notepad++ was already open
+            // put a message box to wait for user's confirmation to exit
+            char* name = new char[strlen(lpszMyArgument)+strlen(CAPTION)+1];
+            sprintf(name, "%s%s", CAPTION, lpszMyArgument);
+            MessageBoxA( NULL, "press ok when editing of this file is finished...", name, MB_OK);
+            delete[] name;
+         }
+      }
+   }
+   // on ferme proprement
+   CloseHandle( oProcessInfo.hProcess );
+   CloseHandle( oProcessInfo.hThread );
+   return 0;
+}
---
+++ NppLauncher.nsi
@@ -0,0 +1,177 @@
+;NppLauncher Setup Script
+;--------------------------------
+; Installer for NppLauncher.exe
+; Creation : 2013-01-24
+; Author: Mattes H. mattesh(at)gmx.net
+;
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either
+; version 2 of the License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;--------------------------------
+
+
+!ifndef VERSION
+  !define VERSION '0.9.3'
+!endif
+
+;--------------------------------
+;Configuration
+
+!ifdef OUTFILE
+  OutFile "${OUTFILE}"
+!else
+  OutFile ..\NppLauncher-${VERSION}-setup.exe
+!endif
+
+SetCompressor /SOLID lzma
+
+InstallDir $PROGRAMFILES\NppLauncher
+InstallDirRegKey HKLM Software\NppLauncher ""
+
+RequestExecutionLevel admin
+
+;--------------------------------
+;Header Files
+
+!include "MUI2.nsh"
+!include "Sections.nsh"
+!include "LogicLib.nsh"
+!include "Memento.nsh"
+!include "WordFunc.nsh"
+
+;--------------------------------
+;Definitions
+
+!define SHCNE_ASSOCCHANGED 0x8000000
+!define SHCNF_IDLIST 0
+
+;--------------------------------
+;Configuration
+
+;Names
+Name "NppLauncher"
+Caption "NppLauncher ${VERSION} Setup"
+
+;Memento Settings
+!define MEMENTO_REGISTRY_ROOT HKLM
+!define MEMENTO_REGISTRY_KEY "Software\Microsoft\Windows\CurrentVersion\Uninstall\NppLauncher"
+
+;Interface Settings
+!define MUI_ABORTWARNING
+
+!define MUI_HEADERIMAGE
+; !define MUI_WELCOMEFINISHPAGE_BITMAP ".\NppLauncher.bmp"
+
+!define MUI_COMPONENTSPAGE_SMALLDESC
+
+;Pages
+!define MUI_WELCOMEPAGE_TITLE "Welcome to the NppLauncher ${VERSION} Setup Wizard"
+!define ILINE1 "This wizard will install NppLauncher ${VERSION}."
+!define ILINE2 "The program replaces notepad.exe by Notepad++.exe."
+!define ILINE3 "It ensures that an instance call to notepad opens the file and a dialog box to block the caller."
+!define MUI_WELCOMEPAGE_TEXT "${ILINE1}$\r$\n${ILINE2}$\r$\n${ILINE3}$\r$\n$\r$\n$_CLICK"
+
+!insertmacro MUI_PAGE_WELCOME
+!insertmacro MUI_PAGE_LICENSE ".\license.txt"
+; !ifdef VER_MAJOR & VER_MINOR & VER_REVISION & VER_BUILD
+; Page custom PageReinstall PageLeaveReinstall
+; !endif
+!insertmacro MUI_PAGE_COMPONENTS
+!insertmacro MUI_PAGE_DIRECTORY
+!insertmacro MUI_PAGE_INSTFILES
+
+!define MUI_FINISHPAGE_LINK "Visit the NppLauncher site for the latest news, FAQs and support"
+!define MUI_FINISHPAGE_LINK_LOCATION "http://NppLauncher.sourceforge.net/"
+
+!insertmacro MUI_PAGE_FINISH
+
+!insertmacro MUI_UNPAGE_CONFIRM
+!insertmacro MUI_UNPAGE_INSTFILES
+
+;--------------------------------
+;Languages
+
+!insertmacro MUI_LANGUAGE "English"
+
+;--------------------------------
+;Installer Sections
+Section "NppLauncher" SecCore
+
+  SetDetailsPrint textonly
+  DetailPrint "Installing NppLauncher ..."
+  SetDetailsPrint listonly
+
+  SectionIn 1 RO
+  SetOutPath $INSTDIR
+
+  SetOverwrite on
+  File .\Release\NppLauncher.exe
+  File .\license.txt
+
+  WriteRegStr HKLM "Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" "Debugger" "$INSTDIR\NppLauncher.exe"
+  WriteRegStr HKLM "Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" "readme" "call NppLauncher.exe instead of original notepad.exe! To disable this option just remove notepad.exe entry"
+  WriteRegStr HKLM "Software\NppLauncher" "cmd" "$PROGRAMFILES\Notepad++\notepad++.exe"
+  WriteRegStr HKLM "Software\NppLauncher" "WaitForClose" "yes"
+  WriteRegStr HKLM "Software\NppLauncher" "Debug" "no"
+  WriteRegDWORD HKLM "Software\NppLauncher" "WaitTimeMsec" 200
+
+  System::Call 'Shell32::SHChangeNotify(i ${SHCNE_ASSOCCHANGED}, i ${SHCNF_IDLIST}, i 0, i 0)'
+
+SectionEnd
+
+Section -post
+
+  WriteUninstaller $INSTDIR\uninst-NppLauncher.exe
+
+SectionEnd
+
+;--------------------------------
+;Descriptions
+
+!insertmacro MUI_FUNCTION_DESCRIPTION_BEGIN
+  !insertmacro MUI_DESCRIPTION_TEXT ${SecCore} "The core files required to use NppLauncher."
+!insertmacro MUI_FUNCTION_DESCRIPTION_END
+
+;--------------------------------
+;Uninstaller Section
+
+Section Uninstall
+
+  SetDetailsPrint textonly
+  DetailPrint "Uninstalling NppLauncher..."
+  SetDetailsPrint listonly
+
+  IfFileExists $INSTDIR\NppLauncher.exe NppLauncher_installed
+    MessageBox MB_YESNO "It does not appear that NppLauncher is installed in the directory '$INSTDIR'.$\r$\nContinue anyway (not recommended)?" IDYES NppLauncher_installed
+    Abort "Uninstall aborted by user"
+  NppLauncher_installed:
+
+  SetDetailsPrint textonly
+  DetailPrint "Deleting Registry Keys..."
+  SetDetailsPrint listonly
+
+  System::Call 'Shell32::SHChangeNotify(i ${SHCNE_ASSOCCHANGED}, i ${SHCNF_IDLIST}, i 0, i 0)'
+
+  DeleteRegKey HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\NppLauncher"
+  DeleteRegKey HKLM "Software\NppLauncher"
+  DeleteRegKey HKLM "Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe"
+
+  SetDetailsPrint textonly
+  DetailPrint "Deleting Files..."
+  SetDetailsPrint listonly
+ 
+  RMDir /r $INSTDIR
+
+  SetDetailsPrint both
+
+SectionEnd
