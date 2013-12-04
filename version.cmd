aa4d0ef67e515b4d8ab43399badf637e60e8d6c5
Yonggang Luo <luoyonggang@gmail.com>
2013-12-05 01:33:07 +0800

    No need to generate installer.

--- version.cmd
+++
@@ -1,6 +0,0 @@
-cd /d %1
-copy /Y "versionInfo.tags" "versionInfo.inc"
-if "%SVNDIR%" equ "" (
-set SVNDIR=%ProgramW6432%\TortoiseSVN\bin
-)
-rem "%SVNDIR%\SubWCRev.exe" "." "versionInfo.tags" "versionInfo.inc"
